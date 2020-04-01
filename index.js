5e833137deaa1dad2aeaff184cb9f3c6ed4778d3
Mark Norrish <mgnorrish@gmail.com>
2020-04-01 20:46:20 +1100

    added animation on event

---
+++ index.js
@@ -0,0 +1,39 @@
+/*jshint esversion: 6 */
+
+buttons = document.querySelectorAll(".drum");
+soundFiles = {
+  w: "tom-1",
+  'a': "tom-2",
+  's': "tom-3",
+  'd': "tom-4",
+  'j': "snare",
+  'k': "kick-bass",
+  'l': "crash"
+};
+
+function playKey(key) {
+  if (Object.keys(soundFiles).includes(key)) {
+    try {
+      var btn = document.querySelector("."+key);
+      btn.classList.add("pressed");
+      setTimeout(() => {  btn.classList.remove("pressed"); }, 100);
+    } catch (e) {
+      console.log("missing HTML button " + key);
+    }
+    try {
+      f = "sounds/" + soundFiles[key] + ".mp3";
+      new Audio(f).play();
+    } catch (e) {
+      console.log("missing audio file " + f);
+    }
+  }
+}
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    playKey(this.innerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  playKey(event.key);
+});
